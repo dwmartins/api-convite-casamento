5f718402f8a8346f8ae056b0bd528be99fb1db30
Douglas Martins <douglas5422@outlook.com>
2023-09-01 22:45:01 -0300

    feat: removendo a porta do banco

--- config/db.js
+++ config/db.js
@@ -11,7 +11,6 @@ class DatabaseConnection {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
-            port: process.env.DB_PORT
         });
     }
 
